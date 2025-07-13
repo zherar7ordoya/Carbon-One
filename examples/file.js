990f7a4e1f0c3f12d42e8401cab68f5390062c17
Gerardo Tordoya <zherar7ordoya@outlook.com>
2025-07-13 16:44:04 -0300

    Again...

--- examples/file.js
+++ examples/file.js
@@ -1,4 +1,9 @@
 class Rectangle {
+
+    // Declare properties
+    height;
+    width;
+
     constructor(height, width) {
         this.height = height;
         this.width = width;
@@ -20,6 +25,11 @@ class Rectangle {
 }
 
 const square = new Rectangle(10, 10);
+
+// Example of a function that uses the Rectangle class
+function createSquare(sideLength) {
+    return new Rectangle(sideLength, sideLength);
+}
 
 console.log(square.area); // 100
 console.log([...square.getSides()]); // [10, 10, 10, 10]
